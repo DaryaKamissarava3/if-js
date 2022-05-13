b0ad9065db685c2ae23de74bc2cdf7a17f6c4cff
Darya <dashakomiss37@gmail.com>
2022-05-13 16:11:09 +0300

    Change test.js for colors

--- tests/colors.test.js
+++ tests/colors.test.js
@@ -1,7 +1,6 @@
-document.body.innerHTML =
-    `<p id="text1"> +
-    <p id="text2"> +
-    <p id="text3">`;
+/**
+ * @jest-environment jsdom
+ */
 
 const changeColor = () => {
     let i = 0;
@@ -15,52 +14,56 @@ const changeColor = () => {
 }
 
 describe('check the click on p tag', () => {
+    document.body.innerHTML =
+        ' <p id="text1">' +
+        '<p id="text2">' +
+        '<p id="text3">';
 
     const element1 = document.getElementById('text1');
     const element2 = document.getElementById('text2');
     const element3 = document.getElementById('text3');
-
-    const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
     const elements = [element1, element2, element3];
 
+    const colors = ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'];
+
     test('expect color magenta', async () => {
-       for (let i=0;i<elements.length;i++){
-           elements[i].click();
-           changeColor(elements[i]);
-           const color=elements[i].style.color;
-           expect(color).toBe('magenta')
-       }
-    });
-    test('expect color cyan', async () => {
-        for (let i=0;i<elements.length;i++){
+        for (let i = 0; i < elements.length; i++) {
             elements[i].click();
             changeColor(elements[i]);
-            const color=elements[i].style.color;
-            expect(color).toBe('cyan')
+            const color = colors[0];
+            expect(color).toBe('magenta');
+        }
+    });
+    test('expect color cyan', async () => {
+        for (let i = 0; i < elements.length; i++) {
+            elements[i].click();
+            changeColor(elements[i]);
+            const color = colors[1];
+            expect(color).toBe('cyan');
         }
     });
     test('expect color firebrick', async () => {
-        for (let i=0;i<elements.length;i++){
+        for (let i = 0; i < elements.length; i++) {
             elements[i].click();
             changeColor(elements[i]);
-            const color=elements[i].style.color;
-            expect(color).toBe('firebrick')
+            const color = colors[2];
+            expect(color).toBe('firebrick');
         }
     });
     test('expect color springgreen', async () => {
-        for (let i=0;i<elements.length;i++){
+        for (let i = 0; i < elements.length; i++) {
             elements[i].click();
             changeColor(elements[i]);
-            const color=elements[i].style.color;
-            expect(color).toBe('springgreen')
+            const color = colors[3];
+            expect(color).toBe('springgreen');
         }
     });
     test('expect color skyblue', async () => {
-        for (let i=0;i<elements.length;i++){
+        for (let i = 0; i < elements.length; i++) {
             elements[i].click();
             changeColor(elements[i]);
-            const color=elements[i].style.color;
-            expect(color).toBe('skyblue')
+            const color = colors[4];
+            expect(color).toBe('skyblue');
         }
     });
 });
