6227535feb0c8be532329d43ccaa8f10a52d2d7f
Darya <dashakomiss37@gmail.com>
2022-06-13 22:21:04 +0300

    change spaces

--- src/main.js
+++ src/main.js
@@ -1,28 +1,32 @@
 const colors = {
-    data: ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'],
-    [Symbol.iterator]() {
-        let nextIndex = 0;
-        const next = () => {
-            if (nextIndex < this.data.length) {
-                const value = this.data[nextIndex++];
-                return {value, done: false};
-            }
-            if (nextIndex === this.data.length) {
-                nextIndex = 0;
-                const value = this.data[nextIndex++];
-                return {value, done: false};
-            }
-            return {done: true};
-        };
-        return {next};
-    }
-}
+  data: ['magenta', 'cyan', 'firebrick', 'springgreen', 'skyblue'],
+  [Symbol.iterator]() {
+    let nextIndex = 0;
+
+    const next = () => {
+
+      if (nextIndex < this.data.length) {
+        const value = this.data[nextIndex++];
+        return { value, done: false };
+      }
+
+      if (nextIndex === this.data.length) {
+        nextIndex = 0;
+        const value = this.data[nextIndex++];
+        return { value, done: false };
+      }
+
+      return { done: true };
+    };
+    return { next };
+  },
+};
 
 for (let p of document.getElementsByTagName('p')) {
-    let i = 0;
-    const it = colors[Symbol.iterator]();
-    p.onclick = function () {
-        this.style.color = it.next().value;
-        i++;
-    }
+  let i = 0;
+  const it = colors[Symbol.iterator]();
+  p.onclick = function () {
+    this.style.color = it.next().value;
+    i++;
+  };
 }
