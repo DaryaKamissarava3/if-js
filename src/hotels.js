cea3f34eb9e4475c3f444b28a049428ee8fc75bd
Darya <dashakomiss37@gmail.com>
2022-07-04 15:36:48 +0300

    change fetch and function

--- src/hotels.js
+++ src/hotels.js
@@ -1,44 +1,48 @@
 let sectionForAvailableHotels = document.getElementById('section');
+let destinationInput = document.getElementById('destination');
 
-function showInformation(str, hotels) {
-  let arr = hotels.filter((el) => el.country === str || el.city === str || el.name === str);
-  console.log(arr);
-  arr.forEach((item) => {
-    if (arr.length !== 0) {
-      sectionForAvailableHotels.className = 'section';
-      let divContainer = document.createElement('div');
-      let sectionHeader = document.createElement('div');
-      let sectionSubtitle = document.createElement('h2');
-      let divForHotels = document.createElement('div');
-      divContainer.className = 'container';
-      sectionForAvailableHotels.appendChild(divContainer);
-      sectionHeader.className = 'section-header';
-      divContainer.appendChild(sectionHeader);
-      sectionSubtitle.className = 'section-subtitle underline';
-      sectionSubtitle.textContent = 'Available Hotels';
-      sectionHeader.appendChild(sectionSubtitle);
-      divForHotels.innerHTML = `<img class="card-img col-s-12" src="${item.imageUrl}" alt="picture isn't load">
-    <div class="card-hostel-name">${item.name}</div>
-    <div class="card-hostel-location">${item.city}, ${item.country}</div>`;
-      divContainer.appendChild(divForHotels);
-      return true;
-    } else {
-      alert('does not exist');
-    }
-  });
+document.getElementById('search-btn').addEventListener('click', showInformation);
+
+const onSearch = async (strForSearch) => {
+  const response = await fetch(
+    ` https://fe-student-api.herokuapp.com/api/hotels?search=${strForSearch}`
+  );
+  return response.json();
+};
+
+async function showInformation() {
+  let textInInput = destinationInput.value.toLowerCase();
+
+  const info = await onSearch(textInInput);
+
+  if (info.length === 0) {
+    alert('No such hotel');
+  } else {
+    sectionForAvailableHotels.className = 'section search-hotels-section';
+    let divContainer = document.createElement('div');
+    let sectionHeader = document.createElement('div');
+    let sectionSubtitle = document.createElement('h2');
+    divContainer.className = 'container search-hotels-container';
+    sectionForAvailableHotels.appendChild(divContainer);
+    sectionHeader.className = 'section-header';
+    divContainer.appendChild(sectionHeader);
+    sectionSubtitle.className = 'section-subtitle underline';
+    sectionSubtitle.textContent = 'Available Hotels';
+    sectionHeader.appendChild(sectionSubtitle);
+
+    info.forEach((item) => {
+      if (
+        item.country.toLowerCase() === textInInput ||
+        item.city.toLowerCase() === textInInput ||
+        item.name.toLowerCase() === textInInput
+      ) {
+        let divForHotels = document.createElement('div');
+        divForHotels.className = 'search-hotels';
+        divForHotels.innerHTML = `<img class="card-img col-s-12" src="${item.imageUrl}" alt="picture isn't load">
+                    <div class="card-hostel-name">${item.name}</div>
+                    <div class="card-hostel-location">${item.city}, ${item.country}</div>`;
+        divContainer.appendChild(divForHotels);
+      }
+    });
+  }
 }
-function onChange() {
-  fetch('https://fe-student-api.herokuapp.com/api/hotels')
-      .then((response) => response.json())
-      .then((result) => {
-        console.log(result);
-        let destinationInput = document.getElementById('destination');
-        let textInInput = destinationInput.value;
-        console.log(textInInput);
-
-        console.log(showInformation(textInInput, result));
-      });
-}
-document.getElementById('search-btn').addEventListener('click', onChange);
-
-
