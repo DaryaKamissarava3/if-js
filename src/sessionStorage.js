6e956624d762072820009a29422df188a64faab5
Darya <dashakomiss37@gmail.com>
2022-07-12 08:37:33 +0300

    add try-catch

--- src/sessionStorage.js
+++ src/sessionStorage.js
@@ -1,25 +1,30 @@
 const cardWithPictures = document.getElementById('card');
 
 const arrFromFetch = async () => {
-  const response = await fetch(`https://fe-student-api.herokuapp.com/api/hotels/popular`);
-  return response.json();
+    try {
+        const response = await fetch(`https://fe-student-api.herokuapp.com/api/hotels/popular`);
+        return response.json();
+    } catch (error) {
+        console.log(error.message);
+    }
 };
 
 async function checkSessionStorage() {
-  if (sessionStorage.getItem('arrayOfHotels') === null) {
-    const info = await arrFromFetch();
-    sessionStorage.setItem('arrayOfHotels', JSON.stringify(info));
-  }
-  let result = sessionStorage.getItem('arrayOfHotels');
-  JSON.parse(result).forEach((item) => {
-    const newCardItem = document.createElement('div');
-    newCardItem.className = 'card-item';
-    newCardItem.innerHTML = `
+    let hotels = sessionStorage.getItem('arrayOfHotels')
+    if (!hotels) {
+        const hotels = await arrFromFetch();
+        sessionStorage.setItem('arrayOfHotels', JSON.stringify(hotels));
+    }
+    let result = sessionStorage.getItem('arrayOfHotels');
+    JSON.parse(result).forEach((item) => {
+        const newCardItem = document.createElement('div');
+        newCardItem.className = 'card-item';
+        newCardItem.innerHTML = `
            <img class="card-img col-s-12" src="${item.imageUrl}" alt="picture isn't load">
            <div class="card-hostel-name">${item.name}</div>
            <div class="card-hostel-location">${item.city}, ${item.country}</div>`;
-    cardWithPictures.appendChild(newCardItem);
-  });
+        cardWithPictures.appendChild(newCardItem);
+    });
 }
 
 checkSessionStorage().catch((error) => console.log(error));
