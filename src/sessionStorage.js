5cf7691011db2860cd87114edc485ddb3a6728e5
Darya <dashakomiss37@gmail.com>
2022-07-12 08:21:32 +0300

    change function

--- src/sessionStorage.js
+++ src/sessionStorage.js
@@ -9,28 +9,17 @@ async function checkSessionStorage() {
   if (sessionStorage.getItem('arrayOfHotels') === null) {
     const info = await arrFromFetch();
     sessionStorage.setItem('arrayOfHotels', JSON.stringify(info));
-    let result = sessionStorage.getItem('arrayOfHotels');
-    JSON.parse(result).forEach((item) => {
-      const newCardItem = document.createElement('div');
-      newCardItem.className = 'card-item';
-      newCardItem.innerHTML = `
-           <img class="card-img col-s-12" src="${item.imageUrl}" alt="picture isn't load">
-           <div class="card-hostel-name">${item.name}</div>
-           <div class="card-hostel-location">${item.city}, ${item.country}</div>`;
-      cardWithPictures.appendChild(newCardItem);
-    });
-  } else {
-    let resultFromStorage = sessionStorage.getItem('arrayOfHotels');
-    JSON.parse(resultFromStorage).forEach((item) => {
-      const newCardItem = document.createElement('div');
-      newCardItem.className = 'card-item';
-      newCardItem.innerHTML = `
-           <img class="card-img col-s-12" src="${item.imageUrl}" alt="picture isn't load">
-           <div class="card-hostel-name">${item.name}</div>
-           <div class="card-hostel-location">${item.city}, ${item.country}</div>`;
-      cardWithPictures.appendChild(newCardItem);
-    });
   }
+  let result = sessionStorage.getItem('arrayOfHotels');
+  JSON.parse(result).forEach((item) => {
+    const newCardItem = document.createElement('div');
+    newCardItem.className = 'card-item';
+    newCardItem.innerHTML = `
+           <img class="card-img col-s-12" src="${item.imageUrl}" alt="picture isn't load">
+           <div class="card-hostel-name">${item.name}</div>
+           <div class="card-hostel-location">${item.city}, ${item.country}</div>`;
+    cardWithPictures.appendChild(newCardItem);
+  });
 }
 
 checkSessionStorage().catch((error) => console.log(error));
