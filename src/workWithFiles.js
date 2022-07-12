0c47a3cf0d97e7bb52a4b159e9f8f7664c5fed14
Darya <dashakomiss37@gmail.com>
2022-07-12 09:58:01 +0300

    remake part of the function

--- src/workWithFiles.js
+++ src/workWithFiles.js
@@ -9,15 +9,10 @@ formEl.addEventListener('submit', async event => {
         enctype: 'multipart/form-data',
         body: fd,
     };
-
-    const res = await fetch('https://fe-student-api.herokuapp.com/api/file', fetchOptions)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(response.statusText);
-            }
-            return response.json();
-        })
-        .then(result => result)
-        .catch(error => console.log(error.message));
-    console.log(res);
+    try {
+        const res = await fetch('https://fe-student-api.herokuapp.com/api/file', fetchOptions);
+        console.log(res.json());
+    } catch (error) {
+        console.log(error)
+    }
 });
