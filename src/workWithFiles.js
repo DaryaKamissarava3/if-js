9dbe76c415a6d1f3dc42de3e87c8509a6b94b721
Darya <dashakomiss37@gmail.com>
2022-07-12 09:59:15 +0300

    add space after fetchOptions

--- src/workWithFiles.js
+++ src/workWithFiles.js
@@ -1,18 +1,19 @@
 const formEl = document.getElementById('form');
 
-formEl.addEventListener('submit', async event => {
-    event.preventDefault();
-    let fd = new FormData(formEl);
+formEl.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  let fd = new FormData(formEl);
 
-    const fetchOptions = {
-        method: 'Post',
-        enctype: 'multipart/form-data',
-        body: fd,
-    };
-    try {
-        const res = await fetch('https://fe-student-api.herokuapp.com/api/file', fetchOptions);
-        console.log(res.json());
-    } catch (error) {
-        console.log(error)
-    }
+  const fetchOptions = {
+    method: 'Post',
+    enctype: 'multipart/form-data',
+    body: fd,
+  };
+
+  try {
+    const res = await fetch('https://fe-student-api.herokuapp.com/api/file', fetchOptions);
+    console.log(res.json());
+  } catch (error) {
+    console.log(error);
+  }
 });
